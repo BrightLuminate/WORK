049f23a152dfa605d245a44823ecfcb9fbedbdf3
BrightLuminate <grace71394@gmail.com>
2024-04-25 17:54:05 +0900

    config 부터 from_file을 사용 방법 까지

--- apps/app.py
+++ apps/app.py
@@ -9,14 +9,29 @@ from apps.config import config
 db = SQLAlchemy()   # 객체 생성 db로 다룰것이다.
 csrf = CSRFProtect() 
 
+#import json  from_file 를 사용하는 방법
+
+# def create_app(): config를 읽어 들이는 다른 방법 from_pyfile 를 사용하는 방법
+
 # create_app 함수를 작성한다.
 # config의 키를 전달한다.
 def create_app(config_key):
     # 플라스크 인스턴스 생성
     app = Flask(__name__)
+
+    #앱의 config 설정 from_file 사용하는 방법
+    #app.config.from_file("config.json", load=json.load)
+
+    
+    # 앱의cofing 설정  from_pyfile 를 사용하는 방법
+    # app.config.from_pyfile("config.py")
+
+    # from_envvar 를 사용하는 방법
+    #app.config.from_envvar("APPLICATION_SETTINGS")
+
   # config_key에 매치하는 환경의 config 클래스를 읽어 들인다.
     app.config.from_object(config[config_key])
-# 앱의 config 설정을 한다.
+# 앱의 config 설정을 한다. 만약에 config 읽어들이는 방법은 app.config.from_object(config[config_key]) 제외 한다.
     app.config.from_mapping(
       SECRET_KEY="2AZSMss3p5QPbcYBsJ", # 자체적으로 이름 정할수도 있다.
         SQLALCHEMY_DATABASE_URI=
